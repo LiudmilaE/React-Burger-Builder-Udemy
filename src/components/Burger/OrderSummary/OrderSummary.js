e6dc387c2566e0019139006d7728efc4c001b43c
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-02-12 10:34:20 +0300

    Change folders structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //this can be a functional component
+    componentWillUpdate() {
+        console.log('[OrderSummary] willUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(ingKey => {
             return (
             <li key={ingKey}>
                 <span style={{textTransform: 'capitalize'}}>
-                {ingKey}</span>: {props.ingredients[ingKey]}
+                {ingKey}</span>: {this.props.ingredients[ingKey]}
             </li>)
         });
-    return (
-        <React.Fragment>
-            <h3>Your order</h3>
-            <p>A delicious burger with following ingredients: </p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </React.Fragment>
-    )
+        return (
+            <React.Fragment>
+                <h3>Your order</h3>
+                <p>A delicious burger with following ingredients: </p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </React.Fragment>
+        )
+    } 
 };
 
-export default orderSummary;
+export default OrderSummary;
