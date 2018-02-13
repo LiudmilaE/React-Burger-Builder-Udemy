cc5546160bb681c73fa149974c8d3c7b9daba0f3
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-02-13 11:53:13 +0300

    Add validation styling and feedback to form data contacts

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,6 +15,11 @@ class ContactData extends Component {
                     placeholder: 'Your Name'
                 },
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -23,6 +28,11 @@ class ContactData extends Component {
                     placeholder: 'Your Email'
                 },
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,6 +41,11 @@ class ContactData extends Component {
                     placeholder: 'Your Street'
                 },
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -39,6 +54,13 @@ class ContactData extends Component {
                     placeholder: 'Your Zip Code'
                 },
                 value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -47,6 +69,11 @@ class ContactData extends Component {
                     placeholder: 'Your Country'
                 },
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -85,6 +112,24 @@ class ContactData extends Component {
             });
     }
 
+    checkValidity (value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -93,6 +138,8 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormEl.value =  event.target.value;
+        updatedFormEl.valid = this.checkValidity(updatedFormEl.value, updatedFormEl.validation);
+        updatedFormEl.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormEl;
         this.setState({orderForm: updatedOrderForm})
     }
@@ -112,7 +159,10 @@ class ContactData extends Component {
                         key={formEl.id}
                         elementType={formEl.config.elementType}
                         elementConfig={formEl.config.elementConfig}
-                        value={formEl.config.value} 
+                        value={formEl.config.value}
+                        invalid={!formEl.config.valid} 
+                        shouldValidate={formEl.config.validation}
+                        touched={formEl.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formEl.id)}/>
                 ))}
                 <Button btnType="Success">ORDER</Button>
