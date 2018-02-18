9d6ede74fc6307d0b024860aa37289951f061c5f
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-02-18 17:45:30 +0300

    Fix few bugs and use env variable to hide devtools in production

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = { 
@@ -120,46 +121,15 @@ class ContactData extends Component {
         
     }
 
-    checkValidity (value, rules) {
-        let isValid = true;
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
-    }
-
-    inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        } 
-        const updatedFormEl = {
-            ...updatedOrderForm[inputIdentifier]
-        };
-        updatedFormEl.value =  event.target.value;
-        updatedFormEl.valid = this.checkValidity(updatedFormEl.value, updatedFormEl.validation);
-        updatedFormEl.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormEl;
-
+    inputChangedHandler = (event, inputIdentifier) => { 
+        const updatedFormEl = updateObject(this.state.orderForm[inputIdentifier], {
+            value : event.target.value,
+            valid : checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            touched : true,
+        });
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier] : updatedFormEl
+        });
         let formIsValid = true;
         for (let inputIdentifier in updatedOrderForm) {
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
